278741734e3c60bc15b6785ddd0d64409d66a83a
Jonathan Hernandez <pgr.gni.20@gmail.com>
2023-01-11 20:56:19 -0600

    ES6 - Strings y Multilinea

---
+++ src/es6/02-strings.js
@@ -0,0 +1,19 @@
+// Clase 5
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!:)';
+console.log(epicPhrase);
+
+// Template literals
+
+let epicPhrase2 = `${hello} ${world}! :)`;
+console.log(epicPhrase2);
+
+// Multi-line strings
+let lorem = 'esto es un string \n' + 'esto es otra linea';
+let lorem2 = `Esto es un string.
+Esto es otra linea.
+`
+
+console.log(lorem);
+console.log(lorem2);
