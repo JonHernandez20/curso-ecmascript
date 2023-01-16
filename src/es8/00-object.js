09dbe193966b750fd33a50b7a1e389cdf264d532
Jonathan Hernandez <pgr.gni.20@gmail.com>
2023-01-16 17:49:34 -0600

    ES8 - Object entries, values y keys

---
+++ src/es8/00-object.js
@@ -0,0 +1,10 @@
+// Clase 19
+
+const objeto = {
+    name: 'Jonathan',
+    age: 19,
+    pass: false
+};
+console.log(Object.entries(objeto));
+console.log(Object.keys(objeto));
+console.log(Object.values(objeto));
