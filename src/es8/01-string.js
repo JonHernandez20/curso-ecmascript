5206ec750f6d555c5cb63dac70383650a6ff9b18
Jonathan Hernandez <pgr.gni.20@gmail.com>
2023-01-16 18:07:15 -0600

    ES8 - Pad y commas

---
+++ src/es8/01-string.js
@@ -0,0 +1,13 @@
+// Clase 20
+
+// Pad
+
+let string = 'Hola mundo';
+
+console.log(string.padStart(15,'!'));
+console.log(string.padEnd(15,'.'));
+
+// Commas 
+const array = [1,2,3, , , , ,];
+console.log(array);
+console.log(array.length);
