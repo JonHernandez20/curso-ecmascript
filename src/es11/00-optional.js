105af8d6a8565e7b405a965132392e00ad915479
Jonathan Hernandez <pgr.gni.20@gmail.com>
2023-01-17 13:52:37 -0600

    ES11 - Propiedades opcionales de un objeto

---
+++ src/es11/00-optional.js
@@ -0,0 +1,16 @@
+// Clase 26
+
+const usuario = {
+    Jon: {
+        color: 'rojo',
+        num: 7
+    },
+    Maria: {
+        color: 'azul',
+        num: 10
+    }
+};
+
+console.log(usuario.Jon.redes);
+console.log(usuario.Jon.redes?.facebook);
+console.log(usuario.Maria.num);
