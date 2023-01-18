2455b24399864de0811e786c87c32fd590965b45
Jonathan Hernandez <pgr.gni.20@gmail.com>
2023-01-18 16:03:25 -0600

    ES12 - replaceAll y numSeparator

---
+++ src/es12/00-replaceAll-numSeparation.js
@@ -0,0 +1,12 @@
+// Clase 31
+
+// separator Numeric
+const number = 100_000_000_000_000;
+console.log(number);
+
+// Replace all
+const string = 'Hola:), he terminado esta clase clapclap.';
+let addEmoji;
+addEmoji = string.replaceAll(':)', '🙂');
+addEmoji = addEmoji.replaceAll('clap', '👏');
+console.log(addEmoji);
